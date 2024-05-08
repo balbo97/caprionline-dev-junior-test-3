a039ccdbe3798ebcfb03702e064c53889fccdab1
balbo97 <balbosauro1@gmail.com>
2024-05-08 20:14:02 +0200

    "Add sorting functionality to MoviesController list method"
    Con questa modifica, se nessun parametro viene specificato nella richiesta, verrà restituito l'elenco dei film senza alcun ordinamento particolare. Se viene specificato il parametro "recent", i film saranno ordinati per anno di uscita in ordine decrescente. Se viene specificato il parametro "rating", i film saranno ordinati per rating in ordine decrescente.

--- backend/src/Controller/MoviesController.php
+++ backend/src/Controller/MoviesController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -13,14 +14,29 @@ class MoviesController extends AbstractController
     public function __construct(
         private MovieRepository $movieRepository,
         private SerializerInterface $serializer
-    ) {}
+    ) {
+    }
 
     #[Route('/movies', methods: ['GET'])]
-    public function list(): JsonResponse
+    public function list(Request $request): JsonResponse
     {
+        // Recupero il criterio di ordinamento dalla query 
+        $orderBy = $request->query->get('orderBy');
+
+        // Recupero i film dal repository
         $movies = $this->movieRepository->findAll();
+
+        // Se è specificato un criterio di ordinamento, ordino i film di conseguenza
+        if ($orderBy === 'recent') {
+            $movies = $this->movieRepository->findBy([], ['year' => 'DESC']);
+        } elseif ($orderBy === 'rating') {
+            $movies = $this->movieRepository->findBy([], ['rating' => 'DESC']);
+        }
+
+
         $data = $this->serializer->serialize($movies, "json", ["groups" => "default"]);
 
+        // Restituisco la risposta JSON
         return new JsonResponse($data, json: true);
     }
 }
